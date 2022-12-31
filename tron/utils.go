74be1e04087b8d6318f03fd3af495a23bcf10596
Command <maoxs2@gmail.com>
2023-01-01 01:05:14 +0800

    fix #5

--- tron/utils.go
+++ tron/utils.go
@@ -15,8 +15,18 @@ func EnsureTAddr(hexStr string) string {
 		return hexStr
 	}
 
+	// if genesis block miner
+	if len(hexStr) == 230 && hexStr == "206e65772073797374656d206d75737420616c6c6f77206578697374696e672073797374656d7320746f206265206c696e6b656420746f67657468657220776974686f757420726571756972696e6720616e792063656e7472616c20636f6e74726f6c206f7220636f6f7264696e6174696f6e" {
+		return "GenesisMiner" //  as a placeholder
+	}
+
+	// if genesis tx
+	if len(hexStr) == 46 && hexStr == "3078303030303030303030303030303030303030303030" {
+		return "7YxAaK71utTpYJ8u4Zna7muWxd1pQwimpGxy8" // = 3078303030303030303030303030303030303030303030
+	}
+
 	if len(hexStr) == 20*2 {
-		hexStr = "41" + hexStr
+		hexStr = "41" + hexStr // make sure the T-prefix
 	}
 
 	if len(hexStr) == 21*2 {
